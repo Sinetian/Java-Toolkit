fa7ca0216436f11cda94298705ec194424c50f95
Sinetian <liujingyu2020@outlook.com>
2023-01-05 23:03:35 +0800

    Update main.java

--- Stable/Stable_v2.3/main.java
+++ Stable/Stable_v2.3/main.java
@@ -12,19 +12,17 @@ public class main {
         System.out.println("** Sinetian's Toolkit **");
         System.out.println("************************");
         System.out.println("版本：Stable v2.3");
+        return;
     }
     public static double cal(int x , int y) {
         return x / y;
     }
     public static void main(String[] args) {
         main.sysinfo();
-        int x , y;
+        int x , y , op;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input number 1:");
-        x = scanner.nextInt();
-        System.out.print("Input number 2:");
-        y = scanner.nextInt();
-        System.out.println(main.cal(x , y));
+        System.out.print("可用功能：\n 1.计算器 2.系统信息 3.退出");
+        op = scanner.nextInt();
 
     }
 }
