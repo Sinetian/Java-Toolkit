2f0dba7576e2360cbbcca210b93124d7650ee9ed
Sinetian <liujingyu2020@outlook.com>
2023-01-05 22:19:10 +0800

    Updated codes.

---
+++ Stable/Stable_v2.3/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void sysinfo() {
+        System.out.println("************************");
+        System.out.println("**    ********        **");
+        System.out.println("**       **           **");
+        System.out.println("**       **           **");
+        System.out.println("**       **    ***    **");
+        System.out.println("**       **    ***    **");
+        System.out.println("************************");
+        System.out.println("** Sinetian's Toolkit **");
+        System.out.println("************************");
+        System.out.println("版本：Stable v2.3");
+    }
+    public static void main(String[] args) {
+        main.sysinfo();
+    }
+}
