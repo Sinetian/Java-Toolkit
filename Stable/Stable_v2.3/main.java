60120500eccd678f671f1c89bee9fd91140d4426
Sinetian <liujingyu2020@outlook.com>
2023-01-05 22:57:24 +0800

    Update main.java

--- Stable/Stable_v2.3/main.java
+++ Stable/Stable_v2.3/main.java
@@ -1,3 +1,5 @@
+import java.util.Formatter;
+import java.util.Scanner;
 public class main {
     public static void sysinfo() {
         System.out.println("************************");
@@ -11,7 +13,18 @@ public class main {
         System.out.println("************************");
         System.out.println("版本：Stable v2.3");
     }
+    public static double cal(int x , int y) {
+        return x / y;
+    }
     public static void main(String[] args) {
         main.sysinfo();
+        int x , y;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number 1:");
+        x = scanner.nextInt();
+        System.out.print("Input number 2:");
+        y = scanner.nextInt();
+        System.out.println(main.cal(x , y));
+
     }
 }
