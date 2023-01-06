db9f9f50424bfba74d10e2c6bfbbda2e5fd7d4c6
Sinetian <liujingyu2020@outlook.com>
2023-01-06 13:34:47 +0800

    Update main.java

--- Stable/Stable_v2.3/main.java
+++ Stable/Stable_v2.3/main.java
@@ -18,22 +18,36 @@ public class main {
         return;
     }
     public static double cal(int x , int y) {
-        return x / y;
+        switch(op) {
+
+        }
     }
     public static void main(String[] args) {
         main.sysinfo();
         int x , y , op;
+        char calu;
         Scanner scanner = new Scanner(System.in);
         while(true) {
-            System.out.println("可用功能：\n 1.计算器 \n 2.系统信息 \n 3.退出");
+            System.out.println("可用功能：\n 1.计算器 2.系统信息 3.退出");
             System.out.print("请输入序号：");
             op = scanner.nextInt();
             switch(op) {
                 case 1: {
+                  System.out.println("计算器使用注意事项：在做减法以及除法时，请确保第一个数比后面的大。");
+                  System.out.println("运算符：+（加）-（减）*（乘）/（除）%（求余）");
                   System.out.print("请输入第一个数字：");
                   x = scanner.nextInt();
                   System.out.print("请输入第二个数字：");
                   y = scanner.nextInt();
+                  System.out.print("请输入运算：");
+                  op = scanner.nextLine();
+                  System.out.printf("%.3f" , main.cal(x , y , op));
+                }
+                case 2: {
+                  main.sysinfo();
+                }
+                case 3: {
+                  System.out.
                 }
             }
         }
